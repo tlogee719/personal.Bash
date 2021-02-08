4d89b500139290068ebdfc5e7908fc40e169bdf9
tlogee719 <44215463+tlogee719@users.noreply.github.com>
2021-02-08 03:09:41 +0000

    Create ssh.sh

---
+++ ssh.sh
@@ -0,0 +1,17 @@
+apt-get install ssh
+service ssh start
+#remove the run levels to enable remote ssh
+update-rc.d -f ssh remove
+#load ssh defaults
+update-rc.d -f ssh defaults
+#update kali defaultkeys to avoid MITM attack
+#move to current keys to a new folder
+cd /etc/ssh/
+mkdir default_kali_keys
+mv ssh_host_* default_kali_keys
+# now I will regenerate the keys
+dpkg-reconfigure openssh-server
+#just for shits and giggles we can verify tht the SSH key hashes are diffrent
+#still in the /etc/shh/ dir
+md5sum ssh_host_*
+
